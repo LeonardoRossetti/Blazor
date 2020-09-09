e417e7cc0de19399a412c4cd72d2ba2308ff7a79
Leonardo <leoo.rossetti@gmail.com>
2020-09-09 16:52:16 +1200

    Initial commit

---
+++ BlazorApp/BlazorWebAssembly/Entities/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Entities
+{
+    public class TaskItem
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public DateTime Creation { get; set; }
+    }
+}
